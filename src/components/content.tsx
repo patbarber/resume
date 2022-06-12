a3accf62e745569be40e665e0efae21e80dd754a
pat <patbarber72@gmail.com>
2022-06-12 21:42:14 +1000

    added icons

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,9 +1,31 @@
 import type { Component } from "solid-js";
+import { createSignal, For } from "solid-js";
 
 const Content: Component = () => {
+  const [skills, setSkills] = createSignal([
+    { name: "Javascript", icon:"javascript-plain.svg" },
+    { name: "Typescript", icon:"typescript-plain.svg"  },
+    { name: "C#", icon:"dot-net-plain-wordmark.svg"  },
+    { name: "Java", icon:"java-plain.svg"  },
+    { name: "React", icon:"react-original.svg"  },
+    { name: "Vuejs", icon:"vuejs-original.svg"  },
+    { name: "Angular", icon:"angularjs-original.svg"  },
+    { name: "Electron", icon:"electron-original.svg"  },
+  ]);
+
   return (
-    <div class="h-screen">
-      <p  class="m-auto w-fit text-2xl">Skills</p>
+    <div class="h-screen font-mono">
+      <p class="m-auto w-fit text-4xl mb-4">Skills</p>
+      <div class="grid grid-cols-1 md:grid-cols-2 m-auto w-fit text-center">
+        <For each={skills()}>
+          {(skill, i) => (
+            <div class="m-2 text-xl grid grid-cols-2">
+              <img class="h-12" src={`./src/assets/${skill.icon}`}/>
+              <p class="m-auto">{skill.name}</p>
+            </div>
+          )}
+        </For>
+      </div>
     </div>
   );
 };
--- src/components/typewriter.tsx
+++ src/components/typewriter.tsx
@@ -4,7 +4,7 @@ export default function Typerwriter(props) {
   return (
     <>
       <div class="w-fit">
-        <p class="typed-out text-4xl text-white font-mono">~ {props.text}</p>
+        <p class="typed-out text-2xl md:text-4xl text-white font-mono">~ {props.text}</p>
       </div>
     </>
   );
