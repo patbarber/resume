0500b0ebd22e32f931d84a201b176f5b7fce594b
Pat Barber <patbarber72@gmail.com>
2022-07-28 21:54:37 +1000

    added memo

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,5 +1,5 @@
 import type { Component } from "solid-js";
-import { createSignal, For } from "solid-js";
+import { createSignal, For, createMemo } from "solid-js";
 import images from "./images";
 import linkedin from "../assets/socials/linkedin-original.svg";
 import javascript from "../assets/skills/javascript-plain.svg";
@@ -17,44 +17,78 @@ const Experience: Component = () => {
       name: "Vood",
       icons: [images.typescript, react],
       postion: "Front end developer",
+      description: "ecommerce",
+      dates: "at some point",
+      Company: "annex",
     },
     {
       name: "Department of Education, Skills and Employment",
       icons: [javascript, vuejs, dotnet],
       postion: "Fullstack developer",
+      description: "ecommerce333",
+      dates: "at some point",
+      Company: "annex",
     },
     {
       name: "Services Australia",
       icons: [typescript, angularjs, java],
       postion: "Fullstack developer",
+      description: "ecommerce22",
+      dates: "at some point",
+      Company: "annex",
     },
     {
       name: "Aurabox",
       icons: [typescript, react, electron],
       postion: "Front end developer",
+      description: "ecommerce1212",
+      dates: "at some point",
+      Company: "annex",
     },
   ]);
+
+  const [selectedExp, setSelectedExp] = createSignal();
+
+  const select = (exp: any) => {
+    console.log(exp);
+    const element = <div>hello</div>;
+    return setSelectedExp(exp);
+  };
+
+  const expSelected = createMemo(() => selectedExp());
 
   return (
     <div class="h-screen">
       <div class="w-fit m-auto">
-      <p class="w-fit text-4xl mb-4 m-auto">Experience</p>
+        <p class="w-fit text-4xl mb-4 m-auto">Experience</p>
 
-        <div class="m-auto p-4 grid gap-4  md:max-w-3xl">
-          <For each={experience()}>
-            {(experience, i) => (
-              <div class="hover:bg-neutral-900">
-                <p class="text-2xl">{experience.name}</p>
-                <p>Role: {experience.postion}</p>
-                <p class="">Technology used: </p>
-                <div class="grid grid-rows-1 grid-flow-col w-fit space-x-1">
-                  <For each={experience.icons}>
-                    {(icon, i) => <img class="h-12" src={icon} />}
-                  </For>
+        <div class="m-auto p-4 grid grid-cols-3 gap-4  md:max-w-3xl">
+          <div class="col-span-1">
+            <For each={experience()}>
+              {(exp, i) => (
+                <div
+                  onclick={() => select(exp)}
+                  class="hover:bg-neutral-900 cursor-pointer"
+                >
+                  <p class="text-2xl">{exp.name}</p>
+                  <p>Role: {exp.postion}</p>
+                  <p class="">Technology used: </p>
+                  <div class="grid grid-rows-1 grid-flow-col w-fit space-x-1">
+                    <For each={exp.icons}>
+                      {(icon, i) => <img class="h-12" src={icon} />}
+                    </For>
+                  </div>
                 </div>
-              </div>
-            )}
-          </For>
+                /*      <button >
+                </button> */
+              )}
+            </For>
+          </div>
+          <div class="hover:bg-neutral-900 col-span-2">
+            <p>Project Description: {expSelected()?.description}</p>
+            <p>Dates:</p>
+            <p>Company:</p>
+          </div>
         </div>
       </div>
     </div>
